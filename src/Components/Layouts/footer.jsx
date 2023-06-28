af3e69a5c9c16da2ec2989e66b76b1740f720d7d
fred2541 <fredionze@free.fr>
2023-06-28 13:36:03 +0200

    Convert all picture from ScreenProjet to webp

--- src/Components/Layouts/footer.jsx
+++ src/Components/Layouts/footer.jsx
@@ -2,9 +2,9 @@ const Footer = () => {
     return (        
         <footer className="footer">
             <p>
-                <a href="#"><img src="/Assets/Icons/Facebook_f_logo_(2019).svg" alt="Logo" /></a>
-                <a href="#"><img src="/Assets/Icons/LinkedIn_icon.svg" alt="Logo" /></a>
-                <a href="https://github.com/fred2541" target="_blank" rel="noreferrer"><img src="/Assets/Icons/Octicons-mark-github.svg" alt="Logo" /></a>
+                <a href="https://www.facebook.com/" target="_blank" rel="noreferrer"><img src="/Assets/Icons/Facebook_f_logo_(2019).svg" alt="Logo" height="25" width="25"/></a>
+                <a href="https://www.linkedin.com" target="_blank" rel="noreferrer"><img src="/Assets/Icons/LinkedIn_icon.svg" alt="Logo" height="25" width="25"/></a>
+                <a href="https://github.com/fred2541" target="_blank" rel="noreferrer"><img src="/Assets/Icons/Octicons-mark-github.svg" alt="Logo" height="25" width="25"/></a>
                 </p>
             <p>Réalisation: Bourigault Frederic</p>
             <p>© 2023 Tous droits réservés</p>
